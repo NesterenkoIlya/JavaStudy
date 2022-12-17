dce7cc83366bf47ee752594c92e242406840805f
Athet <55511806+NesterenkoIlya@users.noreply.github.com>
2022-12-17 00:25:27 +0000

    initial commit

---
+++ Test/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static int Sum (int a, int b) {
+        int i = a + b;
+        return i;
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
